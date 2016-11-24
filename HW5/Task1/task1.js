b40eaf506a9107eaea400e5cdaf49036fd91f40d
Valentin Timchenko <vtimchenko87@gmail.com>
2016-11-24 23:29:05 +0200

    change js file in according to Anastasia code after review

--- HW5/Task1/task1.js
+++ HW5/Task1/task1.js
@@ -7,35 +7,27 @@
 
 
 (function () {
-    var current;
     var container = document.querySelector(".accordion-container");
 
     container.addEventListener("click", function (e) {
-        var target = e.target;
+        var target = e.target, current;
 
         if (!target.classList.contains("item")) {
             return;
         }
 
-        if (target.classList.contains("show")) {
-            current = null;
-            target.classList.toggle("show");
-            target.firstChild.classList.toggle("show");
-            target.nextElementSibling.classList.toggle("show");
-        } else {
+        if (!target.classList.contains("show")) {
+            current = document.querySelector(".show");
             if (current) {
                 current.classList.toggle("show");
                 current.firstChild.classList.toggle("show");
                 current.nextElementSibling.classList.toggle("show");
             }
-            current = target;
-            current.classList.toggle("show");
-            current.firstChild.classList.toggle("show");
-            current.nextElementSibling.classList.toggle("show");
         }
+        target.classList.toggle("show");
+        target.firstChild.classList.toggle("show");
+        target.nextElementSibling.classList.toggle("show");
     });
-
 })();
-
 
 

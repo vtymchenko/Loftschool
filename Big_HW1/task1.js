5a4eb80681f4260d530e3c3478bc1d2cd85fbcb8
Valentin Timchenko <vtimchenko87@gmail.com>
2017-01-02 02:08:03 +0200

    d&d html5, localStorage implementation

--- Big_HW1/task1.js
+++ Big_HW1/task1.js
@@ -1,3 +1,9 @@
+(function () {
+
+    userFilteredData = [];
+    allDataList = [] ;
+    var userDataList = [] ;
+
 new Promise(function(resolve) {
     if (document.readyState == 'complete') {
         resolve();
@@ -20,75 +26,131 @@ new Promise(function(resolve) {
         });
     }).then(function() {
         return new Promise(function(resolve, reject) {
-
-         VK.api('friends.get', {'fields':'bdate,photo_50'}, response => {
+            VK.api('friends.get', {'fields':'bdate,photo_50'}, response => {
                 if (response.error) {
-                reject(new Error(response.error.error_msg));
-            } else
-            {
-                var userFilteredData =[];
-                var userDataList = response.response;
-                var filteredDataList = [] ;
-
-
-                var source = vkFriendsTemplate.innerHTML;
-                var templateFn = Handlebars.compile(source);
-                var template = templateFn({list: userDataList});
-                allFriendsContainer.innerHTML = template;
-
-                var allSearch = document.querySelector(".allSearch");
-
-        allSearch.addEventListener("input", function (e) {
-            var filteredArr = [];
-            var index = 0 ;
-            var target = e.target  ;
-            if (target.value) {
-                for (var i = 0; i < userDataList.length; i++) {
-                    if (userDataList[i].first_name.toLowerCase().indexOf(target.value.toLowerCase()) !== -1) {
-                        filteredArr[index] = userDataList[i];
-                        index++;
-                        continue;
-                    }
+                    reject(new Error(response.error.error_msg));
                 }
+            else {
+                    var allFriendsContainer = document.getElementById("allFriendsContainer");
+                    var filteredFriendsContainer = document.getElementById("filteredFriendsContainer");
+                    var saveBtn = document.getElementById("save");
+                    var allSearch = document.querySelector(".allSearch");
+                    var filteredSearch = document.querySelector(".filteredSearch");
+                    var template;
 
-                var template = templateFn({list: filteredArr});
-                allFriendsContainer.innerHTML = template;
-             } else {
-                var template = templateFn({list: userDataList});
-                allFriendsContainer.innerHTML = template;
-            }
-        });
+                    allDataList = response.response;
+                    userDataList = response.response;
+                    var source = vkFriendsTemplate.innerHTML;
+                    var templateFn = Handlebars.compile(source);
 
-                allFriendsContainer.addEventListener("click", function (e) {
-                    var target = e.target ;
+                    if (localStorage.getItem("filteredList")) {
 
-                    if(target.classList.contains("add-favorite-pic")){
-                        var newEl = document.body.appendChild(target.closest("li")) ;
-                        newEl.setAttribute("draggable", "false");
-                        filteredFriendsContainer.appendChild(newEl);
-                     }
-                });
+                        userFilteredData = JSON.parse(localStorage.getItem("filteredList"));
+                        for (var i = 0; i < userDataList.length; i++) {
+                            for (var j = 0; j < userFilteredData.length; j++) {
+                                if (userDataList[i].uid == userFilteredData[j].uid) {
+                                    userDataList.splice(i, 1);
+                                }
+                            }
+                        }
+                        template = templateFn({list: userFilteredData});
+                        filteredFriendsContainer.innerHTML = template;
 
-                filteredFriendsContainer.addEventListener("click", function (e) {
-                    var target = e.target ;
-                    if(target.classList.contains("add-favorite-pic")){
-                        var removedEl = document.body.appendChild(target.closest("li")) ;
-                        removedEl.setAttribute("draggable", "true");
-                        allFriends.appendChild(removedEl);
                     }
+
+                    userDataList.sort(function (objA, objB) {
+                        if (objA.first_name > objB.first_name) {
+                            return 1;
+                        }
+
+                        if (objA.first_name == objB.first_name) {
+                            return 0;
+                        }
+                        return -1;
+                    });
+
+                    template = templateFn({list: userDataList});
+                    allFriendsContainer.innerHTML = template;
+
+                    filteredSearch.addEventListener("input", function (e) {
+                        filterSearch(e, userFilteredData, filteredFriendsContainer)
+                    });
+
+                    allSearch.addEventListener("input", function (e) {
+                        filterSearch(e, userDataList, allFriendsContainer)
+                    });
+
+                    function filterSearch(e, userData, container) {
+                        var filteredArr = [];
+                        var index = 0;
+                        var target = e.target;
+                        if (target.value) {
+                            for (var i = 0; i < userData.length; i++) {
+                                if (userData[i].first_name.toLowerCase().indexOf(target.value.toLowerCase()) !== -1) {
+                                    filteredArr[index] = userData[i];
+                                    index++;
+                                    continue;
+                                }
+                            }
+
+                            template = templateFn({list: filteredArr});
+                            container.innerHTML = template;
+                        } else {
+                            template = templateFn({list: userData});
+                            container.innerHTML = template;
+                        }
+                    }
+
+                    allFriendsContainer.addEventListener("click", function (e) {
+                        var target = e.target;
+                        var element = target.closest("li");
+                        if (target.classList.contains("add-favorite-pic")) {
+                            element.setAttribute("draggable", "false");
+                            for (var i = 0; i < userDataList.length; i++) {
+                                if (userDataList[i].uid == element.id) {
+                                    userFilteredData.push(userDataList[i]);
+                                    userDataList.splice(i, 1);
+                                }
+                            }
+                            filteredFriendsContainer.appendChild(element);
+                        }
+                    });
+
+
+                    filteredFriendsContainer.addEventListener("click", function (e) {
+                        var target = e.target;
+                        var element = target.closest("li");
+                        if (target.classList.contains("add-favorite-pic")) {
+                            element.setAttribute("draggable", "true");
+                            for (var i = 0; i < userFilteredData.length; i++) {
+                                if (userFilteredData[i].uid == element.id) {
+                                    userDataList.push(userFilteredData[i]);
+                                    userFilteredData.splice(i, 1);
+                                }
+                            }
+                            allFriendsContainer.appendChild(element);
+                        }
+                    });
+
+                    saveBtn.addEventListener("click", function () {
+                        if (userFilteredData) {
+                            localStorage.clear();
+                            localStorage.setItem("filteredList", JSON.stringify(userFilteredData));
+                        }
+                    });
+
+                    resolve();
+                }
                 })
-
-                resolve();
-            }
             })
-        });
+        }).catch(function(e) {
+                alert(`Ошибка: ${e.message}`);
+            });
+})();
 
-    }).catch(function(e) {
-        alert(`Ошибка: ${e.message}`);
-    });
 
 function allowDrop(ev) {
-     ev.preventDefault();
+    ev.preventDefault();
 }
 
 function drag(ev) {
@@ -98,8 +160,11 @@ function drag(ev) {
 function drop(ev) {
     ev.preventDefault();
     var data = ev.dataTransfer.getData("text");
-    ev.target.appendChild(document.getElementById(data));
+    var element = document.getElementById(data) ;
+    ev.target.appendChild(element);
+    for (var i = 0; i < allDataList.length; i++) {
+        if ( allDataList[i].uid == data) {
+            userFilteredData.push(allDataList[i]);
+        }
+    }
 }
-
-
-
